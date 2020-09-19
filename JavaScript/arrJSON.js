d87fc417a515614c816b204beb2f01320668bf4d
stepin104310 <69225700+stepin104310@users.noreply.github.com>
2020-09-19 18:07:15 +0530

    Add files via upload

---
+++ JavaScript/arrJSON.js
@@ -0,0 +1,17 @@
+let arr = [10,20,30];
+for( let i in arr)
+{
+    console.log(val);
+}
+
+for(const val of "Javascript")
+{
+    console.log(val)
+}
+
+arr.forEach(function(value,index){
+    console.log(value,index)
+});
+
+arr.forEach(ele.index) => console.log("element", ele, "index", index));
+var newArr = new Array(2,3)
